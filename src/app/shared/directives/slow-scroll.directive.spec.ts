73de853837f37678b9906d25b22b627175597942
@ohadxd <ohadcochavi@gmail.com>
2023-02-07 07:50:29 +0200

    First commit

---
+++ src/app/shared/directives/slow-scroll.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SlowScrollDirective } from './slow-scroll.directive';
+
+describe('SlowScrollDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SlowScrollDirective();
+    expect(directive).toBeTruthy();
+  });
+});
